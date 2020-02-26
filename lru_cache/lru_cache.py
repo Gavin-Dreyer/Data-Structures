3980baa2ac01ca981bc8fd72c62bd34b4645903b
Gavin Dreyer <gavindreyer1@gmail.com>
2020-02-25 16:10:18 -0800

    lru_cache mvp

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,8 @@
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('./doubly_linked_list.py')
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +11,10 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.storage = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,9 +23,14 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
-    def get(self, key):
-        pass
 
+    def get(self, key):
+        if self.storage.__len__() > 0:
+            (cur_node, val) = self.storage.get_value(key)
+            self.storage.move_to_end(cur_node)
+            return val
+        else:
+            return None
     """
     Adds the given key-value pair to the cache. The newly-
     added pair should be considered the most-recently used
@@ -29,5 +41,15 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        entry = {key: value}
+
+        if self.storage.__len__() < self.limit:
+            self.storage.add_to_tail(entry)
+        else:
+            if self.storage.check_dll_to_update(key, value) == True:
+                self.storage.check_dll_to_update(key, value)
+            else:
+                self.storage.remove_from_head()
+                self.storage.add_to_tail(entry)
